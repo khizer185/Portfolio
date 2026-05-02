d7c42cd4e3673892fcf4b97c65700f8d445de826
Muhammad Khizer <muhammadkhizer185@gmail.com>
2026-05-02 04:12:34 +0100

    hero section completed

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
                 document.getElementById('click').checked = false;
             });
         });
+
+        var typed = new Typed(".auto-type" , {
+            strings: ["Frontend Developer","Mern Stack Developer", "React Developer"],
+            typeSpeed:150,
+            backSpeed:150,
+            loop:true
+        })
